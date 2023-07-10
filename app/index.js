cec42ff76db72e75b76dae3f13f598ad84cbbbd1
arunp406 <arunpalamuri406@gmail.com>
2023-07-10 20:14:02 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -8,7 +8,7 @@ import {Router,browserHistory} from 'react-router';
 
 import {Provider} from 'react-redux';
 import routes from './routes';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import configureStore from './store/configureStore';
 
